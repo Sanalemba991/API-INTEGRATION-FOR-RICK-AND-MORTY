93f9d7797f04591bd77215beb243897b8be66492
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-08 20:46:21 +0530

    Adding Class

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className='home'>
-      <div className='h1home'><h1  > Welcome to Rick and Mory Pages</h1></div>
+      <div className='h1home'><h1  > Welcome to Rick and Mory Application</h1></div>
         
-     <Link to="/character">Character</Link>
+        <div className='homeCa'> <Link to="/character">Character</Link></div>
+     
     </div>
   )
 }

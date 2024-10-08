3840970bb88c392745fd3cefff45a22273b7476b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-08 21:00:53 +0530

    correct morty name

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className='home'>
-      <div className='h1home'><h1  > Welcome to Rick and Mory Application</h1></div>
+      <div className='h1home'><h1  > Welcome to Rick and Morty Application</h1></div>
         
         <div className='homeCa'> <Link to="/character">Character</Link></div>
      

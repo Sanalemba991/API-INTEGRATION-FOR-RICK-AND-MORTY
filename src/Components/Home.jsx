1e67a2e3291bc3f6fa892d7db18f8ffef1102fc2
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-09 22:21:15 +0530

    Adding

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,10 +5,11 @@ function Home() {
   return (
     <div className='home'>
       <div className='h1home'><h1  > Welcome to Rick and Morty Application</h1></div>
-        
+        <div className='Sm'>
         <div className='homeCa'> <Link to="/character">Character</Link></div>
         <div className='homeCa'> <Link to="/episode">Episode</Link></div>
         <div className='homeCa'> <Link to="/location">Location</Link></div>
+        </div>
     </div>
   )
 }

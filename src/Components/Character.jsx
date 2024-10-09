2cf3ecfb1683c2b231d501be7805d6a688f0d555
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-09 21:06:09 +0530

    Search Bar Input

--- src/Components/Character.jsx
+++ src/Components/Character.jsx
@@ -1,37 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './character.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./character.css";
+
 function Character() {
   const [rick, setRick] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/character")
-      .then(response => {
-        setRick(response.data.results); 
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        setRick(response.data.results);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []); 
+  }, []);
+
+  // For Searching
+  const filteredCharacters = rick.filter(character =>
+    character.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <>
-      <div className='As'>
-        <h1 className='name'>Rick and Morty Characters</h1>
-        <div className='cha'>
-        <ul>
-          {rick.map(character => (
-            <li key={character.id} >
-              <h2>{character.name}</h2>
-              <img src={character.image} alt={character.name} />
-              <p>Status: {character.status}</p>
-              <p>Species: {character.species}</p>
-              <p>Gender: {character.gender}</p>
-              <p>Origin: {character.origin.name}</p>
-              <p>Location: {character.location.name}</p>
-            </li>
-          ))}
-        </ul>
+      <div className="As">
+        <h1 className="name">Rick and Morty Characters</h1>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search characters"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+     
+        </div>
+        <div className="cha">
+          <ul>
+            {filteredCharacters.map((character) => (
+              <li key={character.id}>
+                <h2>{character.name}</h2>
+                <img src={character.image} alt={character.name} />
+                <p>Status: {character.status}</p>
+                <p>Species: {character.species}</p>
+                <p>Gender: {character.gender}</p>
+                <p>Origin: {character.origin.name}</p>
+                <p>Location: {character.location.name}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>

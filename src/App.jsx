ac09cfd90e2026bc8986d33584e51c7c2088beec
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-12 10:25:12 +0530

    Using useParam Hook Try to integrate From Scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,45 @@
-
-
-
 import {
   createBrowserRouter,
-  Outlet,
   RouterProvider,
 } from "react-router-dom";
 import Character from "./Components/Character";
 import Read from "./Components/Read";
 import Home from "./Components/Home";
 import Episode from "./Components/Episode";
-
 import Location from "./Components/Location";
 
+// Create the router
 const router = createBrowserRouter([
   {
-   
-      path:"/",
-      element:<Home/>
-
-
-      }, 
-  
-  
-  
-  {
-    path:"/character",
-    element:<Character/>,
+    path: "/",
+    element: <Home />,
   },
- 
   {
-    path:"/episode",
-    element:<Episode/>,
+    path: "/character",
+    element: <Character />,
   },
- 
   {
-    path:"/location",
-    element:<Location/>,
+    path: "/episode",
+    element: <Episode />,
   },
-   
   {
-    path:"/Read",
-    element:<Read/>,
-  }
- 
- 
-
-  
-
-
-
+    path: "/location",
+    element: <Location />,
+  },
+  {
+    path: "/read/:id", 
+    element: <Read />,
+  },
 ]);
+
 function App() {
   return (
-   <div className='app'>
-    <div className='container'>
-       <RouterProvider router={router}/>
-  </div>
-   </div>)
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
-
-
 
 export default App;

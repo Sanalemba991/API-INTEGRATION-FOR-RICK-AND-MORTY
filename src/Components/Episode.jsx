80aa9c922cb856ab035f901219fef2ebdd7d237a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-20 20:42:28 +0530

    button

--- src/Components/Episode.jsx
+++ src/Components/Episode.jsx
@@ -54,11 +54,11 @@ const Episode = () => {
           ))}
         </ul>
         <div className="pagination">
-          <button onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))} disabled={currentPage === 1}>
+          <button  className="btn" onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))} disabled={currentPage === 1}>
             Previous
           </button>
           <span>Page {currentPage} of {totalPages}</span>
-          <button onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages}>
+          <button className="ctn"  onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages}>
             Next
           </button>
         </div>
